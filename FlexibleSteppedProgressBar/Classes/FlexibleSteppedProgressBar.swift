720ad5d77b7004c760f066124a88e0a265b994a2
m7md3elwan <eng.m.elwan@gmail.com>
2019-07-27 01:55:16 +0200

    Fix 0 index not being selected

--- FlexibleSteppedProgressBar/Classes/FlexibleSteppedProgressBar.swift
+++ FlexibleSteppedProgressBar/Classes/FlexibleSteppedProgressBar.swift
@@ -752,7 +752,7 @@ import CoreGraphics
             
             
             
-            if(self.currentIndex != selectedIndex) || selectedIndex == 0 {
+            if ((self.currentIndex != selectedIndex) || (self.currentIndex == 0)) {
                 if let canSelect = self.delegate?.progressBar?(self, canSelectItemAtIndex: selectedIndex) {
                     if (canSelect) {
                         if (selectedIndex > completedTillIndex) {
